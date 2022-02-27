4fe71f3e263a6f6a01e6c13fc2501b5fec8442f3
IvanDamNation <ivan.suvalkin@gmail.com>
2022-02-27 11:54:26 +0300

    Transfer models to news app

--- accounts/models.py
+++ accounts/models.py
@@ -1,21 +1,2 @@
 from django.db import models
 
-
-class Author(models.Model):
-    pass
-
-
-class Category(models.Model):
-    pass
-
-
-class Post(models.Model):
-    pass
-
-
-class PostCategory(models.Model):
-    pass
-
-
-class Comment(models.Model):
-    pass
